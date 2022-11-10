fd698ca29c53ff10285e92bbd4a3beb2b0238aa4
david <david@Debian.myguest.virtualbox.org>
2022-11-09 22:35:13 -0500

    responsive menu added

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -10,21 +10,25 @@ const Header = ({titulo,logo}) =>(
     <header className='Header'>
         <div className='contenido'>
             <div className='Logo'>
-                <a href="/"><img className="main_logo" src={logo} alt="icon" /></a>
-                <h1>{titulo}</h1>
+                <a href="index.html "><img className="main_logo" src={logo} alt="icon" /></a>
+                <h1 className='name'>{titulo}</h1>
             </div>
-            <nav>
-                <NavLink id='HederLink' to = "/">Home</NavLink>
-                <NavLink id='HederLink' to = "/">Projects</NavLink>
-                <NavLink id='HederLink' to = "/">Education</NavLink>
-                <NavLink id='HederLink' to = "/">About</NavLink> 
-                <NavLink id='HederLink' to = "/">contact</NavLink> 
+            <input type="checkbox" id="check"/>
+            <label htmlFor="check" className="checkbtn">
+                <div className="linkContainer">
+                    <div className="line"></div>
+                    <div className="line"></div>
+                    <div className="line"></div>
+                </div>
+            </label>
+            <nav className='nav'>
+                <NavLink id='HederLink' to = "index.html" onChange={Header}>Home</NavLink>
+                <NavLink id='HederLink' to = "" onClick={CloseEvent} >Projects</NavLink>
+                <NavLink id='HederLink' to = "/" onClick={CloseEvent} >Education</NavLink>
+                <NavLink id='HederLink' to = "/" onClick={CloseEvent} >About</NavLink> 
+                <NavLink id='HederLink' to = "/" onClick={CloseEvent} >contact</NavLink> 
             </nav>
-            <div className="linkContainer">
-                <div className="line">  </div>
-                <div className="line"></div>
-                <div className="line"></div>
-            </div>
+            
         </div> 
     </header>
     )
